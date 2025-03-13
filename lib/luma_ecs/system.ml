639744550104374ea238e89a8948593bdb36ab38
Steven <dev@stevenm.co>
2025-03-13 12:22:37 +1100

    Use plural

--- lib/luma_ecs/system.ml
+++ lib/luma_ecs/system.ml
@@ -1,10 +1,10 @@
-type ('w, 'a) without_resource = {
+type ('w, 'a) without_resources = {
   filter : Query.Filter.t;
   query : 'a Query.t;
   run : 'w -> (Id.Entity.t * 'a) list -> 'w;
 }
 
-type ('w, 'a, 'b) with_resource = {
+type ('w, 'a, 'b) with_resources = {
   filter : Query.Filter.t;
   query : 'a Query.t;
   resource_query : 'b Resource.Resource_query.t;
@@ -12,18 +12,18 @@ type ('w, 'a, 'b) with_resource = {
 }
 
 type ('w, 'a) t =
-  | WithoutResources : ('w, 'a) without_resource -> ('w, 'a) t
-  | WithResources : ('w, 'a, 'b) with_resource -> ('w, 'a) t
+  | WithoutResources : ('w, 'a) without_resources -> ('w, 'a) t
+  | WithResources : ('w, 'a, 'b) with_resources -> ('w, 'a) t
 
 let make
     ?(filter = Query.Filter.Any)
     (query : 'a Query.t)
-    (run_fn : 'w -> (Id.Entity.t * 'a) list -> 'w) : ('w, 'a) without_resource =
+    (run_fn : 'w -> (Id.Entity.t * 'a) list -> 'w) : ('w, 'a) without_resources =
   { filter; query; run = run_fn }
 
 let make_with_resources
     ?(filter = Query.Filter.Any)
     (query : 'a Query.t)
     (resource_query : 'b Resource.Resource_query.t)
-    (run_fn : 'w -> (Id.Entity.t * 'a) list -> 'b -> 'w) : ('w, 'a, 'b) with_resource =
+    (run_fn : 'w -> (Id.Entity.t * 'a) list -> 'b -> 'w) : ('w, 'a, 'b) with_resources =
   { filter; query; resource_query; run = run_fn }
--- lib/luma_ecs/system.mli
+++ lib/luma_ecs/system.mli
@@ -1,17 +1,17 @@
-type ('w, 'a) without_resource = {
+type ('w, 'a) without_resources = {
   filter : Query.Filter.t;
   query : 'a Query.t;
   run : 'w -> (Id.Entity.t * 'a) list -> 'w;
 }
-(** [without_resource] represents a system that does not require access to any resources. *)
+(** [without_resources] represents a system that does not require access to any resources. *)
 
-type ('w, 'a, 'b) with_resource = {
+type ('w, 'a, 'b) with_resources = {
   filter : Query.Filter.t;
   query : 'a Query.t;
   resource_query : 'b Resource.Resource_query.t;
   run : 'w -> (Id.Entity.t * 'a) list -> 'b -> 'w;
 }
-(** [with_resource] represents a system that runs with the resources returned by the
+(** [with_resources] represents a system that runs with the resources returned by the
     [resource_query]. *)
 
 (** Type [('w, 'a) t] represents a system that can either operate with or without resources.
@@ -20,14 +20,14 @@ type ('w, 'a, 'b) with_resource = {
 
     [WithResources (sys)] represents a system that operates with resources of type ['b]. *)
 type ('w, 'a) t =
-  | WithoutResources : ('w, 'a) without_resource -> ('w, 'a) t
-  | WithResources : ('w, 'a, 'b) with_resource -> ('w, 'a) t
+  | WithoutResources : ('w, 'a) without_resources -> ('w, 'a) t
+  | WithResources : ('w, 'a, 'b) with_resources -> ('w, 'a) t
 
 val make :
   ?filter:Query.Filter.t ->
   'a Query.t ->
   ('w -> (Id.Entity.t * 'a) list -> 'w) ->
-  ('w, 'a) without_resource
+  ('w, 'a) without_resources
 (** Makes a system with no required resources. *)
 
 val make_with_resources :
@@ -35,5 +35,5 @@ val make_with_resources :
   'a Query.t ->
   'b Resource.Resource_query.t ->
   ('w -> (Id.Entity.t * 'a) list -> 'b -> 'w) ->
-  ('w, 'a, 'b) with_resource
+  ('w, 'a, 'b) with_resources
 (** Makes a system with resource represented by type ['b]. *)
