0b0ea1b6ba34679d37a52ae819ad63aaf406a143
Steven <dev@stevenm.co>
2025-09-16 18:07:30 +1000

    Store window config in global resources

--- engine/src/luma_window/window.ml
+++ engine/src/luma_window/window.ml
@@ -1,6 +1,7 @@
 open Luma__driver
 open Luma__ecs
 open Luma__app
+open Luma__resource
 
 module type Window_config = sig
   type colour
@@ -8,10 +9,12 @@ module type Window_config = sig
   type t = {
     height : int;
     width : int;
-    colour : colour option;
+    mutable colour : colour option;
     title : string option;
     resizable : bool;
   }
+
+  module R : Resource.S with type t = t
 
   val default : unit -> t
 
@@ -36,10 +39,16 @@ module Make (D : Driver.S) : S with type colour = D.colour = struct
     type t = {
       height : int;
       width : int;
-      colour : colour option;
+      mutable colour : colour option;
       title : string option;
       resizable : bool;
     }
+
+    module R = Luma__resource.Resource.Make (struct
+      type inner = t
+
+      let name = "window_config_res"
+    end)
 
     let default () =
       { width = 1920; height = 1080; colour = Some D.Colour.white; title = None; resizable = false }
@@ -50,19 +59,24 @@ module Make (D : Driver.S) : S with type colour = D.colour = struct
       { width; height; colour = Some colour; title = Some title; resizable }
   end
 
-  let clear_window (config : Window_config.t) =
-    System.make ~components:End "clear_window" (fun world _ ->
+  let clear_window () =
+    System.make_with_resources ~components:End
+      ~resources:Query.Resource.(Resource (module Window_config.R) & End)
+      "clear_window"
+      (fun world _ (config, _) ->
         let colour = Option.value config.colour ~default:D.Colour.white in
         D.Window.clear colour;
         world)
 
   let init (config : Window_config.t) =
     System.make ~components:End "init" (fun world _ ->
+        let packed = Resource.pack (module Window_config.R) config in
+        let world = World.add_resource Window_config.R.type_id packed world in
         D.Window.init ~width:config.width ~height:config.height
           ~title:(Option.value config.title ~default:"")
           ~resizable:config.resizable;
         world)
 
   let plugin ?(config : Window_config.t = Window_config.default ()) app =
-    app |> App.on PreUpdate (clear_window config) |> App.on PreStartup (init config)
+    app |> App.on PreUpdate (clear_window ()) |> App.on PreStartup (init config)
 end
