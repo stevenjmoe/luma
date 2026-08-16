4a65272e8668827dac52c02e123d9f8775f8b187
Steven <dev@stevenm.co>
2026-08-16 21:58:16 +1000

    define ChildOf hooks that insert/discard relationship components

--- engine/src/luma_ecs/hierarchy.ml
+++ engine/src/luma_ecs/hierarchy.ml
@@ -14,7 +14,7 @@ module ChildOf = struct
 end
 
 module Children = struct
-  type t = Id.Entity.t Dynarray.t
+  type t = Id.Entity.t list
 
   module C = Component.Make (struct
     type inner = t
@@ -22,3 +22,31 @@ module Children = struct
     let name = "children"
   end)
 end
+
+let on_child_of_insert ~entity:child ~command ~component =
+  let parent = ChildOf.parent component in
+
+  Command.modify_entry
+    (module Children.C)
+    parent
+    (function None -> Some [ child ] | Some children -> Some (child :: children))
+    command
+
+let on_child_of_discard ~entity:child ~command ~component =
+  let parent = ChildOf.parent component in
+
+  Command.modify_entry
+    (module Children.C)
+    parent
+    (function
+      | None -> None
+      | Some children ->
+          let children = children |> List.filter (fun c -> not @@ Id.Entity.eq child c) in
+          if children = [] then None else Some children)
+    command
+
+let child_of_hooks : Component_info.Component_hooks.t =
+  {
+    on_insert = Some (Component_info.Component_hook.make (module ChildOf.C) on_child_of_insert);
+    on_discard = Some (Component_info.Component_hook.make (module ChildOf.C) on_child_of_discard);
+  }
--- engine/src/luma_ecs/hierarchy.mli
+++ engine/src/luma_ecs/hierarchy.mli
@@ -3,7 +3,7 @@ open Luma__id
 module ChildOf : sig
   type t
 
-  module C : Component.S
+  module C : Component.S with type t = t
 
   val create : Id.Entity.t -> t
   val parent : t -> Id.Entity.t
@@ -12,5 +12,7 @@ end
 module Children : sig
   type t
 
-  module C : Component.S
+  module C : Component.S with type t = t
 end
+
+val child_of_hooks  : Component_info.Component_hooks.t
